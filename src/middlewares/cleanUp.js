e2953dcae94de45fb27c08c025844cdad00c97c1
iqbal045 <iqbal.ultimate@gmail.com>
2023-04-01 18:10:59 +0600

    - clean up file

--- src/middlewares/cleanUp.js
+++ src/middlewares/cleanUp.js
@@ -2,14 +2,11 @@ const cron = require('node-cron');
 const { File } = require('./../models/File');
 const fs = require('fs');
 
-//'0 */24 * * *';
-
 // Set up file cleanup cron job
 const FILE_INACTIVITY_TIME = 7 * 24 * 60 * 60 * 1000; //  7 days in milliseconds
 const FILE_CLEANUP_PERIOD = '0 */24 * * *'; // runs at 12:00am every day
 
 const cleanUp = cron.schedule(FILE_CLEANUP_PERIOD, async () => {
-  console.log('Running Cron Job');
   const now = new Date();
   const inactivityTimeAgo = new Date(now.getTime() - FILE_INACTIVITY_TIME);
   const inactiveFiles = await File.find({
