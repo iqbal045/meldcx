21f210e5943afc4e6cae9fa5d316a9750ff50627
iqbal045 <iqbal.ultimate@gmail.com>
2023-04-01 17:50:33 +0600

    - clean up

--- src/middlewares/cleanUp.js
+++ src/middlewares/cleanUp.js
@@ -2,10 +2,12 @@ const cron = require('node-cron');
 const { File } = require('./../models/File');
 const fs = require('fs');
 
+//'0 */24 * * *';
 
 // Set up file cleanup cron job
-const FILE_INACTIVITY_TIME = '0 */24 * * *'; // runs at 12:00am every day
-const FILE_CLEANUP_PERIOD = 7 * 24 * 60 * 60 * 1000;  7 days in milliseconds
+const FILE_INACTIVITY_TIME = 7 * 24 * 60 * 60 * 1000; //  7 days in milliseconds
+const FILE_CLEANUP_PERIOD = '0 */24 * * *'; // runs at 12:00am every day
+
 const cleanUp = cron.schedule(FILE_CLEANUP_PERIOD, async () => {
   console.log('Running Cron Job');
   const now = new Date();
@@ -21,7 +23,8 @@ const cleanUp = cron.schedule(FILE_CLEANUP_PERIOD, async () => {
     // delete file from storage
     fs.unlinkSync(file.fileUrl);
   });
-  
+
+  console.log(inactiveFiles);
 });
 
 module.exports = cleanUp;
--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -5,4 +5,5 @@ const upload = require('../helpers/multer');
 router.post('/', upload.single('file'), files);
 router.get('/:publicKey', getFile);
 router.delete('/:privateKey', deleteFile);
+
 module.exports = router;
